52b5712cf45ca69884f335c532ce7ded9d19b19d
Albina MV <alyamarvlad@gmail.com>
2022-05-22 00:46:32 +1000

    Test changed: build should fail

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -17,7 +17,7 @@ public class PostmanEchoTest {
         // Проверки
         .then()
             .statusCode(200)
-                .body("data", equalTo("War is Peace"))
+                .body("data", equalTo("War is war"))
         ;
     }
 
